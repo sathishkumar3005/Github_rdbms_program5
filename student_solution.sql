f78b8b6f1bc044af516f7299c2a4bc15a91656bb
SATHISHKUMAR P <sathishkumar99611@gmail.com>
2026-09-03 09:54:25 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,6 @@
-
+INSERT INTO student58(student80ID,student80name,GENDER,departmentID)
+VALUES
+(1001,'Arun','Male',101),
+(1002,'Divya','Female',102),
+(1003,'Karthik','Male',101);
+SELECT*FROM student80;
